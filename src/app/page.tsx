1b5233216e1025b939b1f121ea5cdda39a5c4745
yashika-yadav123 <yadavyashika0100@gmail.com>
2025-01-31 16:44:07 +0530

    videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Benefits from './components/Benefits'
+import Videos from './components/Videos'
+import Testimonial from './components/Testimonial'
+import Results from './components/Results'
+import DoctorAdvice from './components/DoctorAdvice'
+import KeyIngredients from './components/KeyIngredients'
+import Faq from './components/Faq'
+import Review from './components/Review'
+import Footer from './components/Footer'
 
 const page = () => {
   return (
     <>
-     
+    <Navbar />
+    <Header/>
+    <Benefits/>
+    <Videos/>
+    <Testimonial/>
+    <Results/>
+    <DoctorAdvice/>
+    <KeyIngredients/>
+    <Faq/>
+    <Review/>
+    <Footer/>
     </>
   )
 }
