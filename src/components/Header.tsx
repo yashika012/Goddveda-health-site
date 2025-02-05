542f93b4f7cfb68ec73dbc8fa595163df0d56eb4
devcode8 <devccode8@gmail.com>
2025-02-05 15:57:13 +0530

    fix structure with new element

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -91,7 +91,7 @@ export default function Header() {
                     href="#"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
-                    Log in
+                    Buy Now
                   </a>
                 </div>
               </div>
